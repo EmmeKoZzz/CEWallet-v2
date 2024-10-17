84bc5350b12cd851254dab6cab4972d77740c424
XMKZ <emme.ko.zzz@gmail.com>
2024-10-17 02:56:53 -0400

    fix required details in Transaction Dto

--- ApiServices/DataTransferObjects/TransactionDto.cs
+++ ApiServices/DataTransferObjects/TransactionDto.cs
@@ -6,5 +6,5 @@ public record TransactionDto(
 	[Required] Guid Source,
 	[Required] Guid Currency,
 	[Required] [Range(0, double.MaxValue)] double Amount,
-	[StringLength(255)] string Details
+	[StringLength(255)] string? Details
 );
