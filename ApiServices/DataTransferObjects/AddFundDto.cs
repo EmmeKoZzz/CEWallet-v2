08916fd1ac9947ea0dd4f7d4a8df40be60958b0b
XMKZ <emme.ko.zzz@gmail.com>
2024-10-17 12:30:09 -0400

    details not required

--- ApiServices/DataTransferObjects/AddFundDto.cs
+++ ApiServices/DataTransferObjects/AddFundDto.cs
@@ -2,4 +2,4 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ApiServices.DataTransferObjects;
 
-public record AddFundDto([Required] string Name, string LocationUrl, string Address, string Details);
+public record AddFundDto([Required] string Name, string? LocationUrl, string? Address, string? Details);
--- ApiServices/DataTransferObjects/TransferDto.cs
+++ ApiServices/DataTransferObjects/TransferDto.cs
@@ -3,7 +3,7 @@ using ApiServices.DataTransferObjects.ApiResponses;
 
 namespace ApiServices.DataTransferObjects;
 
-public record TransferDto(Guid Source, [Required] Guid Destination, Guid Currency, double Amount, string Details)
+public record TransferDto(Guid Source, [Required] Guid Destination, Guid Currency, double Amount, string? Details)
 	: TransactionDto(Source, Currency, Amount, Details) {
 	public record Response(FundDto Source, FundDto Destination);
 }
